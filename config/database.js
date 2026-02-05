489ab5d5ade531249c246de740cf6d737fb40163
Codedsleek <codedsleek@gmail.com>
2026-02-05 14:47:55 +0100

    MongoDB init

---
+++ config/database.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+let connected = false;
+
+const connectDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    //if the database is already connected, don't connect again
+    if (connected) {
+        console.log("Already connected to MongoDB");
+        return;
+    }
+
+    // Connect to MongoDB 
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        connected = true;
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+export default connectDB;
