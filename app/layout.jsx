4cec9a1acd6fe93dc476722a28d6f71df9e87509
Codedsleek <codedsleek@gmail.com>
2026-02-07 19:52:57 +0100

    layout fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,7 +19,6 @@ const MainLayout = ({ children }) => {
     </body>
     </html>
 
-
   );
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,6 @@ const HomePage = async () => {
   
   return (
    <>
-   
     <Hero />
     <InfoBoxes />
     <HomeProperties />
